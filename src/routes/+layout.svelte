3658eaf9dbfea74b56bee888c919af1d630a7be1
Colin Bate <colin@colinbate.com>
2025-07-31 16:39:30 -0300

    Make static

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	
+
 	let { children } = $props();
+	// eslint-disable-next-line svelte/valid-prop-names-in-kit-pages
+	export const prerender = true;
 </script>
 
 <svelte:head>
